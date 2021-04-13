491fb4ea8600516de7bd6ddaad65238df5f42dba
Florian Bauer <me@florianbauer.dev>
2021-04-13 13:43:26 +0200

    feat: :sparkles: configure `chakra-ui` with a custom font

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,11 @@
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+import { ChakraProvider } from "@chakra-ui/react";
+import Theme, { Fonts } from "@styles/chakra-theme";
+
+const App = ({ Component, pageProps }) => (
+    <ChakraProvider theme={Theme}>
+        <Fonts />
+        <Component {...pageProps} />
+    </ChakraProvider>
+);
 
 export default App;
---
+++ styles/chakra-theme.js
@@ -0,0 +1,47 @@
+import { extendTheme } from "@chakra-ui/react";
+import { Global } from "@emotion/react";
+
+/**
+ * Using <Global /> from @emotion/react to add styles to the global stylesheet
+ * We're using the @import method to add the font directly in the CSS file.
+ *
+ * <Fonts /> is used in `_app.tsx`
+ */
+export const Fonts = () => (
+    <Global
+        styles={
+            '@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap");'
+        }
+    />
+);
+
+/**
+ * Using the `extendTheme()` to create a custom chakra-configuration
+ * We pass it as `theme` argument on the ChakraProvider in the `_app.tsx`
+ */
+export default extendTheme({
+    fonts: {
+        heading: "Inter, -apple-system, BlinkMacSystemFont, sans-serif",
+        body: "Inter, -apple-system, BlinkMacSystemFont, sans-serif",
+    },
+    fontWeights: {
+        thin: 300,
+        normal: 400,
+        medium: 600,
+        bold: 700,
+    },
+    config: {
+        initialColorMode: "light",
+        useSystemColorMode: false,
+    },
+    styles: {
+        global: () => ({
+            "html, body": {
+                transition: "background 0s",
+            },
+            "html, body, #__next": {
+                height: "100%",
+            },
+        }),
+    },
+});
