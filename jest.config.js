2ce44adb620e4d8fc8bfb17bd2be28514afac04b
Florian Bauer <me@florianbauer.dev>
2021-04-13 16:50:28 +0200

    chore: :wrench: add support for testing

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+    setupFilesAfterEnv: ["<rootDir>/__test__/setupTests.js"],
+    moduleNameMapper: {
+        "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+        "@/components/(.*)": "<rootDir>/components/$1",
+        "@/styles/(.*)": "<rootDir>/components/$1",
+    },
+};
