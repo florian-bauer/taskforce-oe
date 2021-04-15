7799188491971916f996bf4ec45ff0cb69fe7cef
Florian Bauer <me@florianbauer.dev>
2021-04-15 08:51:23 +0200

    test(hooks): :white_check_mark: add unit test to `useOptions` hook

---
+++ hooks/useOptions/index.test.js
@@ -0,0 +1,15 @@
+import { useOptions } from "@/hooks/useOptions";
+
+it("outputs correctly", () => {
+    const definite = "b";
+    const options = [
+        { is: "a", be: 1 },
+        { is: "b", be: 2 },
+        { is: "c", be: 3 },
+        { is: "d", be: 4 },
+    ];
+
+    const output = useOptions(definite, options);
+
+    expect(output).toBe(2);
+});
