6926f2856f300133910524a55e7d06550d6b37ab
Florian Bauer <me@florianbauer.dev>
2021-04-16 11:59:34 +0200

    feat(components): :sparkles: add `filter` component

---
+++ components/filter/RadioCard.js
@@ -0,0 +1,48 @@
+import { Box, Text, useRadio, useStyleConfig } from "@chakra-ui/react";
+import PropTypes from "prop-types";
+
+const RadioCard = ({ label, color, ...props }) => {
+    const { getInputProps, getCheckboxProps } = useRadio(props);
+
+    const input = getInputProps();
+    const checkbox = getCheckboxProps();
+
+    const styles = useStyleConfig("Button", { variant: "solid" });
+
+    return (
+        <Box as="label">
+            <input
+                {...input}
+                style={{ width: 0, height: 0, position: "absolute" }}
+            />
+            <Box
+                {...checkbox}
+                __css={styles}
+                cursor="pointer"
+                userSelect="none"
+                borderRadius={10}
+                fontSize="sm"
+                _active={{ bg: `${color}.300` }}
+                _checked={{
+                    bg: `${color}.100`,
+                    color: `${color}.800`,
+                }}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                transition="background .4s"
+            >
+                <Text fontWeight="medium" whiteSpace="nowrap">
+                    {label.toUpperCase()}
+                </Text>
+            </Box>
+        </Box>
+    );
+};
+
+RadioCard.propTypes = {
+    label: PropTypes.string.isRequired,
+    color: PropTypes.string,
+};
+
+export { RadioCard };
