4bca37a23a2a266ecacd3da478c87c10487f0368
Florian Bauer <me@florianbauer.dev>
2021-04-16 12:07:35 +0200

    feat(storybook): :sparkles: add storybook for `filter` component

---
+++ components/filter/index.stories.js
@@ -0,0 +1,35 @@
+import { Filter as FilterComponent } from "@/components/filter";
+
+export default {
+    title: "Filter",
+    component: FilterComponent,
+};
+
+const Template = (args) => <FilterComponent {...args} />;
+
+export const Filter = Template.bind({});
+Filter.args = {
+    collection: [
+        {
+            label: "Alle",
+            color: "blue",
+        },
+        {
+            label: "Voting",
+            color: "purple",
+        },
+        {
+            label: "In Arbeit",
+            color: "orange",
+        },
+        {
+            label: "Abgearbeitet",
+            color: "green",
+        },
+        {
+            label: "Gelöscht",
+            color: "red",
+        },
+    ],
+    onChange: console.log,
+};
