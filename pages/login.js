a8260d4f1b5f2ea66508a65baabb7f20c01b1e67
Florian Bauer <me@florianbauer.dev>
2021-04-23 11:03:03 +0200

    feat(auth): :sparkles: add error handling for organisation validation

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,38 @@
 import { Button } from "@/components/button";
 import { SignInWithProvider } from "@/lib/auth/provider";
 import { GoogleIcon } from "@/styles/icons/GoogleIcon";
-import { Flex, Image, Skeleton } from "@chakra-ui/react";
+import { Flex, Image, Skeleton, useToast } from "@chakra-ui/react";
 import firebase from "firebase";
 import { AuthAction, withAuthUser } from "next-firebase-auth";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const Login = () => {
+    // Using `useState` and `useEffect` to avoid multiple toast message
+    // caused by multiple re-renders
+    const [_query, setQuery] = useState({});
+
+    const { query } = useRouter();
+    const toast = useToast();
+
+    if (query !== _query) {
+        setQuery(query);
+    }
+
+    useEffect(() => {
+        if (query?.allowed_organisation === "false") {
+            toast({
+                title: "Unauthorisierter Organisationsaccount",
+                description:
+                    "Dein Account muss der BCM Solutions zugeordnet sein!",
+                status: "error",
+                duration: 9000,
+                isClosable: true,
+            });
+        }
+    }, [_query]);
+
     const onLogin = async () =>
         await SignInWithProvider({
             provider: new firebase.auth.GoogleAuthProvider(),
