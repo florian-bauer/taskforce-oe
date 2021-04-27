49e5cd5f861f2d5a3b963d64a043f749daefc087
Florian Bauer <me@florianbauer.dev>
2021-04-27 11:36:45 +0200

    feat(db): :sparkles: add api end points for getting and creating tasks

---
+++ pages/api/tasks/index.js
@@ -0,0 +1,38 @@
+import { createTask, getTasks } from "@/lib/tasks";
+import { verifyIdToken } from "next-firebase-auth";
+
+export default async (req, res) => {
+    let uid;
+    try {
+        // Authorize the User
+        const { id } = await verifyIdToken(req.headers.authorization);
+
+        if (!id) return res.status(401).json({});
+        uid = id;
+    } catch (error) {
+        return res.status(401).json({});
+    }
+
+    const { method } = req;
+
+    // Validating the HTTP Request Method
+    if (!["GET", "POST"].includes(method)) {
+        return res.status(405).json({});
+    }
+
+    if (method === "GET") {
+        const tasks = await getTasks();
+        return res.status(200).json({ tasks });
+    }
+
+    if (method === "POST") {
+        const { title, description } = JSON.parse(req.body);
+
+        await createTask({
+            title,
+            description,
+            uid,
+        });
+        return res.status(200).json({ success: true });
+    }
+};
