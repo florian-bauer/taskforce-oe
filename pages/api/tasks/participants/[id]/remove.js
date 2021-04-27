d904a3dfdba2ab8a102907446b13121cdc73c1aa
Florian Bauer <me@florianbauer.dev>
2021-04-27 13:04:00 +0200

    feat(db): :sparkles: add api endpoint for participant management

---
+++ pages/api/tasks/participants/[id]/remove.js
@@ -0,0 +1,21 @@
+import { removeParticipant } from "@/lib/tasks";
+import { verifyIdToken } from "next-firebase-auth";
+
+export default async (req, res) => {
+    if (req.method !== "POST") {
+        return res.status(405).json({});
+    }
+
+    try {
+        // Authorize the User
+        const { id: uid } = await verifyIdToken(req.headers.authorization);
+
+        if (!uid) return res.status(401).json({});
+
+        const { id } = req.query;
+        await removeParticipant({ id, uid });
+        return res.status(200).json({ success: true });
+    } catch (error) {
+        return res.status(401).json({});
+    }
+};
