734cca49a38cbf542354834dbdeed979d0f310a7
Florian Bauer <me@florianbauer.dev>
2021-05-07 11:58:01 +0200

    refactor(pages): :recycle: improve readabillity of code

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -1,8 +1,10 @@
 import { getUser } from "@/lib/user";
 
 export default async (req, res) => {
-    const { id } = req.query;
+    const { id: userId } = req.query;
 
-    const { _id, ...user } = await getUser({ id });
+    // `_id` is the intern id field of MongoDB
+    // However we're reformatting it to `id` for concistency reasons
+    const { _id, ...user } = await getUser({ id: userId });
     res.send({ id: _id, ...user });
 };
