d35d7410ae5e782af87bfa84f5bdd88173039b4a
Florian Bauer <me@florianbauer.dev>
2021-05-18 12:08:41 +0200

    refactor(controller): :recycle: move `body` component into own file

---
+++ controller/user-list-controller/body.js
@@ -0,0 +1,39 @@
+import { Menu } from "@/components/menu";
+import { getBadgeColor } from "@/controller/user-list-controller/lib";
+import { Avatar, Badge, Flex, Grid, Text } from "@chakra-ui/react";
+
+const Body = ({ name, email, role }) => (
+    <Grid templateColumns="auto 20% 10%" alignItems="center">
+        <Flex w="100%" overflow="hidden">
+            <Avatar size="sm" name={name} src="" />
+            <Flex flexDir="column" ml={2}>
+                <Text fontSize="sm" fontWeight="medium" isTruncated maxW={56}>
+                    {name}
+                </Text>
+                <Text
+                    fontSize="sm"
+                    fontWeight="normal"
+                    color="gray.500"
+                    isTruncated
+                    maxW={56}
+                >
+                    {email}
+                </Text>
+            </Flex>
+        </Flex>
+        <Badge colorScheme={getBadgeColor({ role })} w="max-content">
+            {role}
+        </Badge>
+        <Menu
+            list={[]}
+            menuButtonProps={{
+                background: "gray.900",
+                _hover: { background: "gray.700" },
+                _active: { background: "gray.600" },
+                color: "gray.100",
+            }}
+        />
+    </Grid>
+);
+
+export { Body };
