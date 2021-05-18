2cdcd6a7e48c7ad373464b0135719c6bdd38871b
Florian Bauer <me@florianbauer.dev>
2021-05-18 14:10:06 +0200

    feat(controller): :sparkles: add up and downgrading of user roles

--- controller/user-roles-controller/downgrade-controller.js
+++ controller/user-roles-controller/downgrade-controller.js
@@ -1,23 +1,52 @@
 import { ActionModal } from "@/components/action-modal";
 import { ArrowDownIcon } from "@chakra-ui/icons";
-import { MenuItem } from "@chakra-ui/react";
+import { MenuItem, useToast } from "@chakra-ui/react";
+import { useAuthUser } from "next-firebase-auth";
 
-const DowngradeController = ({ uid, name, email }) => (
-    <ActionModal
-        open={(onOpen) => (
-            <MenuItem icon={<ArrowDownIcon />} onClick={onOpen}>
-                zu User downgraden
-            </MenuItem>
-        )}
-        header="Bist du sicher?"
-        body={`${name} (${email}) wird nun keine uneingeschränkte Berechtigungen auf die Applikation mehr haben.`}
-        labelAbort="Abbrechen"
-        labelAction="Ja, Admin entziehen"
-        onAction={(onClose) => {
-            console.log(uid + " zum user downgraden");
-            onClose();
-        }}
-    />
-);
+const DowngradeController = ({ uid, name, email }) => {
+    const toast = useToast();
+    const { getIdToken } = useAuthUser();
+
+    return (
+        <ActionModal
+            open={(onOpen) => (
+                <MenuItem icon={<ArrowDownIcon />} onClick={onOpen}>
+                    zu User downgraden
+                </MenuItem>
+            )}
+            header="Bist du sicher?"
+            body={`${name} (${email}) wird nun keine uneingeschränkte Berechtigung auf die Applikation mehr haben.`}
+            labelAbort="Abbrechen"
+            labelAction="Ja, Admin Rechte entziehen"
+            onAction={async (onClose) => {
+                const token = await getIdToken();
+
+                const response = await fetch("/api/user/role", {
+                    method: "PUT",
+                    headers: {
+                        authorization: token,
+                    },
+                    body: JSON.stringify({
+                        uid,
+                        administrator: false,
+                    }),
+                });
+
+                const data = await response.json();
+
+                if (data?.success) {
+                    onClose();
+
+                    toast({
+                        title: `${name} ist nun wieder User`,
+                        status: "warning",
+                        duration: 9000,
+                        isClosable: true,
+                    });
+                }
+            }}
+        />
+    );
+};
 
 export { DowngradeController };
---
+++ pages/api/user/role.js
@@ -0,0 +1,30 @@
+import { getUser, setUserRole } from "@/lib/user";
+import { verifyIdToken } from "next-firebase-auth";
+
+export default async (req, res) => {
+    let uid;
+    try {
+        // Authorize the User
+        const { id } = await verifyIdToken(req.headers.authorization);
+
+        if (!id) return res.status(401).json({});
+        uid = id;
+    } catch (error) {
+        return res.status(401).json({});
+    }
+
+    const { method } = req;
+    if (method !== "PUT") return res.status(405).json({});
+
+    const requestUser = await getUser({ id: uid });
+    if (!requestUser?.administrator) return res.status(401).json({});
+
+    // Updating the User Role after token and administrator role is validated
+    const body = JSON.parse(req.body);
+    await setUserRole({
+        uid: body.uid,
+        administrator: body.administrator,
+    });
+
+    return res.status(200).json({ success: true });
+};
