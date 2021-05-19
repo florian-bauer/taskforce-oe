e01d939ed198cf7e7dde3080bef1aaacd2f1a040
Florian Bauer <me@florianbauer.dev>
2021-05-19 12:33:43 +0200

    refactor(controller): :recycle: move participant functions into `@/shared`

--- controller/participant-controller/lib.js
+++ controller/participant-controller/lib.js
@@ -1,26 +1,3 @@
-const request = async ({ token, taskId, action }) => {
-    if (!token || !taskId) return;
-
-    const response = await fetch(
-        `/api/tasks/participants/${taskId}/${action}`,
-        {
-            method: "POST",
-            headers: {
-                authorization: token,
-            },
-        }
-    );
-
-    const data = await response.json();
-    return { data };
-};
-
-const addParticipant = async ({ token, taskId }) => {
-    return await request({ token, taskId, action: "add" });
-};
-const removeParticipant = async ({ token, taskId }) => {
-    return await request({ token, taskId, action: "remove" });
-};
 const isParticipant = ({ participants, uid }) => participants.includes(uid);
 
-export { addParticipant, removeParticipant, isParticipant };
+export { isParticipant };
